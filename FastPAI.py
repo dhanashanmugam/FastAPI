553bf52958b454dc0f74cc2b165f738e20f5c74a
dhanashanmugam <dhanashanmugam@gmail.com>
2025-09-20 17:18:50 +0200

    Create FastPAI.py

---
+++ FastPAI.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+import uvicorn
+app = FastAPI()
+@app.get("/add")
+def add(x,y):
+    return x + y
+@app.get("/subtract")
+def sub(x,y):
+   return x-y
+
+if __name__ == "__main__":
+  uvicorn.run(app,host="0.0.0.0",port=9321)
+ 
